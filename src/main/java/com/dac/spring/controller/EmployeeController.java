95f155813304b8be0f0b8689e98161e3d1a97be6
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-11 16:45:02 +0700

    run UI and disable default form login

--- src/main/java/com/dac/spring/controller/EmployeeController.java
+++ src/main/java/com/dac/spring/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.dac.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,15 @@ public class EmployeeController {
     @GetMapping("/login")
     public String login(){
         return "/login";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(){
+        return "";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "redirect:/employee/login";
     }
 }
